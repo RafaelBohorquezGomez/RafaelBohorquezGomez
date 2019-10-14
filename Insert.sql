b7571202f550477050bb246b1a5a3b0a02c8123c
Anibal Maguilla <anibalmaguilla@gmail.com>
2019-10-14 12:09:40 +0200

    526

--- Insert.sql
+++ Insert.sql
@@ -3,6 +3,6 @@ VALUES ("21832345T","Franciso","Gomez",12,"Neftalí");
 insert into Curso
 VALUES ("1427T","1ªASIR","pepe2000");
 insert into Edificio
-VALUES ("1609C","2ºASIR","tigre222");
+VALUES ("1609C","2ºASIR","tigre22");
 insert INTO AULA
 VALUES ("1548G","1ºASIR","CHACHO");
