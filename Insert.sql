7d0ac9637e8a6a95ad7325a314f5082882c30226
Anibal Maguilla <anibalmaguilla@gmail.com>
2019-10-14 11:57:29 +0200

    Añado el campo Edificio

--- Insert.sql
+++ Insert.sql
@@ -2,3 +2,5 @@ insert into Usuario
 VALUES ("21832345T","Franciso","Gomez",12,"Neftalí");
 insert into Curso
 VALUES ("1427T","1ªASIR","pepe2000");
+insert into Edificio
+VALUES ("1609C","2ºASIR","tigre212")
