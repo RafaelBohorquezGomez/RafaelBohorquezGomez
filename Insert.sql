894ac50d8128c57d8d8b06fa384ee0017c6ec631
Antonio Renque <renquegallego76@gmail.com>
2019-10-16 11:29:40 +0200

    AÑADO INSET AULA

--- Insert.sql
+++ Insert.sql
@@ -2,3 +2,5 @@ insert into Usuario
 VALUES ("21832345T","Franciso","Gomez",12,"Neftalí");
 insert into Curso
 VALUES ("1427T","1ªASIR","pepe2000");
+insert INTO AULA
+VALUES ("1548G","1ºASIR","CHACHO");
